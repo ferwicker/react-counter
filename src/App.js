dbb1e0e347f930b961f14fa3209eb608f06963dd
Fer Wicker <fer.wicker@gmail.com>
2021-09-02 08:16:47 +1000

    basic counter react practice

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Counter from './Counter';
+import CounterHooks from './CounterHooks'
+
+export const ThemeContext = React.createContext(); 
 
 function App() {
-  return null
+  const [theme, setTheme] = useState('red')
+  return (
+    <ThemeContext.Provider value={{ backgroundColor: theme}}>
+      Class component Counter
+      <Counter 
+        initialCount = {0}
+      />
+      Functional component Counter Hooks 
+      <CounterHooks
+      initialCount = {0}
+      />
+      <button onClick={() => setTheme(prevTheme => {
+        return prevTheme === 'red' ? 'blue' : 'red'
+      })}>Toggle theme</button>
+    </ThemeContext.Provider>
+  )
 }
 
 export default App;
